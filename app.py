e1670d71d3ba4831fa2a103c45d3e59ad7196416
YoloSwag2993 <lijize09@gmail.com>
2025-04-06 18:47:58 +0000

    1

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
 from flask import Flask,render_template,request
-import google.generativeai as genai
+import google.generativeai as palm
 
-genai.configure(api_key="AIzaSyD3xCtrHtzi61l9XMA4I9BiPlGXpXzp_5Q",)
+palm.configure(api_key="AIzaSyD3xCtrHtzi61l9XMA4I9BiPlGXpXzp_5Q")
 
-model = genai.GenerativeModel("gemini-pro")
+defaults = { 'model': "models/text-bison-001"}
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
         t = request.form.get("txt")
-        try:
-            response = model.generate_content(t)
-            return render_template("index.html", result=response.text)
-        except Exception as e:
-            return render_template("index.html", result=f"Error: {str(e)}")
+        r = palm.generate_text(**defaults,prompt=t)
+        return(render_template("index.html",result=r.result))
     else:
-        return render_template("index.html", result="waiting")
+        return(render_template("index.html",result="waiting"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
